72a41ce599d745566eb8dcc51ef145b2d920a75b
vampire1696 <duchung.tran@htw-berlin.de>
2024-04-27 18:28:12 +0200

    first commit

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import MobileNav from '../MobileNav'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  return (
+    <nav className={classes.header}>
+      <Gutter className={classes.wrap}>
+        <Link href="/">
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
+        </Link>
+        <HeaderNav header={header} />
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
