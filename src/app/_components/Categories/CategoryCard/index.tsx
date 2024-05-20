5ec4c2f51e0bfb19eec723172ff26bdd73ea80b9
Duc Hung Tran <Duchung.tran@Student.htw-berlin.de>
2024-05-20 19:16:59 +0200

    add category filter provider

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,9 +1,26 @@
+'use client'
 import React from 'react'
+import Link from 'next/link'
+
+import { Category, Media } from '../../../../payload/payload-types'
+import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
 
-const CategoryCard = () => {
-  return <div>CategoryCard</div>
+const CategoryCard = ({ category }: { category: Category }) => {
+  const media = category.media as Media
+  const { setCategoryFilters } = useFilter()
+
+  return (
+    <Link
+      href="/products"
+      className={classes.card}
+      style={{ backgroundImage: `url(${media.url})` }}
+      onClick={() => setCategoryFilters([category.id])}
+    >
+      <p className={classes.title}>{category.title}</p>
+    </Link>
+  )
 }
 
 export default CategoryCard
